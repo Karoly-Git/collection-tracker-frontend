6f09e8526cb6a7e52d8e6a8c6a4f1581c470d552
Karoly Hornyak <karoly.webdev@gmail.com>
2026-01-14 12:49:00 +0000

    Update Loading In Progress icon and increase status icon sizes

--- src/constants/status-icons.js
+++ src/constants/status-icons.js
@@ -1,8 +1,9 @@
 import { FiLogIn, FiLoader, FiCheckCircle, FiLogOut } from "react-icons/fi";
+import { LuForklift } from "react-icons/lu";
 
 export const STATUS_ICONS = {
     CHECKED_IN: FiLogIn,
-    LOADING_IN_PROGRESS: FiLoader,
+    LOADING_IN_PROGRESS: LuForklift,
     LOADED: FiCheckCircle,
     CHECKED_OUT: FiLogOut
 };
