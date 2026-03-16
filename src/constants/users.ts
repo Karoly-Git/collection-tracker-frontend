b165b29a7c4b28c88d5990194ce79dc429530fc7
Karoly Hornyak <karoly.webdev@gmail.com>
2026-03-16 19:23:10 +0000

    create mock users file in constants folder

---
+++ src/constants/users.ts
@@ -0,0 +1,15 @@
+export interface User {
+    name: string;
+    email: string;
+    password: string;
+    loggedIn: boolean;
+}
+
+export const users: User[] = [
+    {
+        name: 'Karoly',
+        email: 'karoly@email.com',
+        password: 'password',
+        loggedIn: true
+    }
+];
